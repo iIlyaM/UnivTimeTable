20c621e4f281430a2143bb0934d634c057fc124e
iIlyaM <imoldavskiy@yandex.ru>
2023-05-04 22:38:53 +0300

    add ``LecturerMainPageFragment``; add icons: ``ic_look_timetable``, ``ic_move_class``, ``ic_send_request``; add ``lecturer_bg.xml``

---
+++ app/src/main/java/vsu/cs/univtimetable/screens/LecturerMainPageFragment.kt
@@ -0,0 +1,29 @@
+package vsu.cs.univtimetable.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
+import vsu.cs.univtimetable.R
+
+
+class LecturerMainPageFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_lecturer_main_page, container, false)
+        val button = view.findViewById<ImageButton>(R.id.prevPageButton)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_lecturerMainPageFragment_to_loginFragment)
+        }
+
+        return view
+    }
+}
