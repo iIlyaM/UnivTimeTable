241e10c27535afa2885d7205894c986cbe8e5a1b
iIlyaM <imoldavskiy@yandex.ru>
2023-05-23 00:12:54 +0300

    add ``AddSubjectPageFragment``

---
+++ app/src/main/java/vsu/cs/univtimetable/screens/lect_screens/AddSubjectPageFragment.kt
@@ -0,0 +1,20 @@
+package vsu.cs.univtimetable.screens.lect_screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import vsu.cs.univtimetable.R
+
+class AddSubjectPageFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_subject_page, container, false)
+    }
+}
