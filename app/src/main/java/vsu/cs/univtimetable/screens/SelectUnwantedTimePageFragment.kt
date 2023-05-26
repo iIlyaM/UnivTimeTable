ff21c8daf14e3ff02670cd6dbca7a54523f2ca8a
iIlyaM <imoldavskiy@yandex.ru>
2023-05-26 22:05:17 +0300

    add ``DayAdapter`` to ``SelectUnwantedTimePageFragment``

--- app/src/main/java/vsu/cs/univtimetable/screens/SelectUnwantedTimePageFragment.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/SelectUnwantedTimePageFragment.kt
@@ -1,5 +1,6 @@
 package vsu.cs.univtimetable.screens
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,31 +10,67 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import vsu.cs.univtimetable.Day
 import vsu.cs.univtimetable.R
+import vsu.cs.univtimetable.screens.adapter.DayAdapter
 
-class SelectUnwantedTimePageFragment : Fragment() {
+class SelectUnwantedTimePageFragment : Fragment(), DayAdapter.OnItemClickListener {
 
-    val days = arrayOf("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота")
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: DayAdapter
 
+    val days = listOf(
+        Day("Понедельник"),
+        Day("Вторник"),
+        Day("Среда"),
+        Day("Четверг"),
+        Day("Пятница"),
+        Day("Суббота")
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        return inflater.inflate(R.layout.fragment_select_unwanted_time_page, container, false)
         val view = inflater.inflate(R.layout.fragment_select_unwanted_time_page, container, false)
-        val autoCompleteView = view.findViewById<AutoCompleteTextView>(R.id.autoCompleteText)
-        val daysAdapter = ArrayAdapter(requireContext(), R.layout.week_item, days)
-        autoCompleteView.setAdapter(daysAdapter)
+        recyclerView = view.findViewById(R.id.weekView)
 
-        autoCompleteView.setOnItemClickListener {
-                parent, view1, position, id ->
-            val selectedItem = parent.getItemAtPosition(position) as String
-            // Действия, которые нужно выполнить при выборе элемента из выпадающего меню
-            // Например, обновление текста или выполнение определенного действия
-        }
-
-
+        adapter = DayAdapter(days)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter.setOnItemClickListener(this)
 
         return view
+    }
+
+    override fun onItemClick(position: Int) {
+        val day = days[position]
+        val options = arrayOf("8:00-9:35", "9:45-11:20", "11:30-13:05", "13:25-15:00", "15:10-16:45", "16:55-18:30")
+        showDialog(day.name, options)
+    }
+
+    private fun showDialog(title: String, options: Array<String>) {
+        val checkedItems = BooleanArray(options.size) { false }
+
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(title)
+            .setMultiChoiceItems(options, checkedItems) { _, which, isChecked ->
+                checkedItems[which] = isChecked
+            }
+            .setPositiveButton("Ок") { dialog, _ ->
+                val selectedOptions = mutableListOf<String>()
+                for (i in options.indices) {
+                    if (checkedItems[i]) {
+                        selectedOptions.add(options[i])
+                    }
+                }
+                // Обработка выбранных вариантов
+            }
+            .setNegativeButton("Отмена") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
