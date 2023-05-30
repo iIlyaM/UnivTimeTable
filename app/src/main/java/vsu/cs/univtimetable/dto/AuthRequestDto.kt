38203e4cc4907ad866d148f018eb4e36e09c5cdb
iIlyaM <imoldavskiy@yandex.ru>
2023-05-30 20:40:10 +0300

    edit ``AuthRequestDto``: 'username' -> 'email'

--- app/src/main/java/vsu/cs/univtimetable/dto/AuthRequestDto.kt
+++ app/src/main/java/vsu/cs/univtimetable/dto/AuthRequestDto.kt
@@ -3,8 +3,8 @@ package vsu.cs.univtimetable.dto
 import com.google.gson.annotations.SerializedName
 
 data class AuthRequestDto(
-    @SerializedName("username")
-    val username: String,
+    @SerializedName("email")
+    val email: String,
 
     @SerializedName("password")
     val password: String
