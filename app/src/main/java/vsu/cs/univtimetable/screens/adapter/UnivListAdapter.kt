ca640b1e9e3eab61fe238099a7bc7ef4be065aed
iIlyaM <imoldavskiy@yandex.ru>
2023-06-01 03:47:59 +0300

    edit ``UnivListAdapter``

--- app/src/main/java/vsu/cs/univtimetable/screens/adapter/UnivListAdapter.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/adapter/UnivListAdapter.kt
@@ -1,39 +1,78 @@
 package vsu.cs.univtimetable.screens.adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import vsu.cs.univtimetable.R
 import vsu.cs.univtimetable.dto.UnivDto
 
-class UnivListAdapter(var context: Context, var univs: List<UnivDto>):
-        RecyclerView.Adapter<UnivListAdapter.UnivViewHolder>() {
-    inner class UnivViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+interface OnUnivItemClickListener {
+    fun onEditClick(univ: UnivDto)
+    fun onDeleteClick(univ: UnivDto)
+}
+
+class UnivListAdapter(
+    var context: Context,
+    var univs: List<UnivDto>,
+    val listener: OnUnivItemClickListener
+) :
+    RecyclerView.Adapter<UnivListAdapter.UnivViewHolder>() {
+    inner class UnivViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var univNameView: TextView
         var cityNameView: TextView
+        var settingsBtn: ImageView
+        var deleteBtn: ImageView
 
         init {
             univNameView = itemView.findViewById(R.id.univNameView)
             cityNameView = itemView.findViewById(R.id.cityNameView)
-        }
+            settingsBtn = itemView.findViewById(R.id.icSettingsView)
+            deleteBtn = itemView.findViewById(R.id.icDeleteView)
 
+            settingsBtn.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onEditClick(univs[position])
+                }
+            }
+
+            deleteBtn.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onDeleteClick(univs[position])
+                }
+            }
+        }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnivViewHolder {
-        return UnivViewHolder(LayoutInflater.from(context).inflate(R.layout.univs_list_item, parent,
-            false))
+        return UnivViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.univs_list_item, parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: UnivViewHolder, position: Int) {
         val obj = univs[position]
         holder.univNameView.text = obj.universityName
         holder.cityNameView.text = obj.city
+        holder.settingsBtn.setOnClickListener { view ->
+            listener.onEditClick(univs[position])
+        }
     }
 
 
     override fun getItemCount(): Int {
         return univs.size
     }
+
 }
