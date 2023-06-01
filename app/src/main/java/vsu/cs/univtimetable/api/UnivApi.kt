d72d3abea5367467c95b842099679a3dce75aa64
iIlyaM <imoldavskiy@yandex.ru>
2023-06-01 03:59:04 +0300

    edit ``UnivApi``: add PUT method

--- app/src/main/java/vsu/cs/univtimetable/api/UnivApi.kt
+++ app/src/main/java/vsu/cs/univtimetable/api/UnivApi.kt
@@ -1,14 +1,27 @@
 package vsu.cs.univtimetable.api
 
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import vsu.cs.univtimetable.dto.UnivDto
 import vsu.cs.univtimetable.dto.UnivResponseDto
 
 interface UnivApi {
 
     @Headers("Content-Type: application/json")
     @GET("universities")
-    fun getUniversities( @Header("Authorization") basicToken: String ): Call<UnivResponseDto>
+    fun getUniversities(@Header("Authorization") basicToken: String): Call<UnivResponseDto>
+
+    @Headers("Content-Type: application/json")
+    @PUT("universities/{id}")
+    fun editUniversity(
+        @Header("Authorization") basicToken: String,
+        @Path("id") id: Int,
+        @Body university: UnivDto
+    ):Call<Void>
 }
