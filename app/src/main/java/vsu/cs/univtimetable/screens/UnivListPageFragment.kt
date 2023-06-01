d9bd4a898310ef2d4e14e0d7f985a29a29f81056
iIlyaM <imoldavskiy@yandex.ru>
2023-06-01 04:00:10 +0300

    edit ``UnivListPageFragment``: add ``update()``

--- app/src/main/java/vsu/cs/univtimetable/screens/UnivListPageFragment.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/UnivListPageFragment.kt
@@ -1,11 +1,14 @@
 package vsu.cs.univtimetable.screens
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -17,9 +20,11 @@ import vsu.cs.univtimetable.TimeTableClient
 import vsu.cs.univtimetable.api.UnivApi
 import vsu.cs.univtimetable.dto.UnivDto
 import vsu.cs.univtimetable.dto.UnivResponseDto
+import vsu.cs.univtimetable.screens.adapter.OnUnivItemClickListener
 import vsu.cs.univtimetable.screens.adapter.UnivListAdapter
+import java.util.zip.Inflater
 
-class UnivListPageFragment : Fragment() {
+class UnivListPageFragment : Fragment(), OnUnivItemClickListener {
 
     private lateinit var univApi: UnivApi
     var data = ArrayList<UnivDto>()
@@ -39,12 +44,13 @@ class UnivListPageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_univ_list_page, container, false)
+        val dialogView = inflater.inflate(R.layout.update_univ_dialog, null)
         recyclerView = view.findViewById(R.id.univsList)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 //        adapter = UnivListAdapter(requireContext(), emptyList())
 //        recyclerView.adapter = adapter
 //        val token:String? = SessionManager.getToken(requireContext())
-        val token:String? = SessionManager.getToken(requireContext())
+        val token: String? = SessionManager.getToken(requireContext())
 //        getUniversities(recyclerView, token)
         return view
     }
@@ -52,19 +58,26 @@ class UnivListPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val token:String? = SessionManager.getToken(requireContext())
+        val token: String? = SessionManager.getToken(requireContext())
         Log.d("API Request failed", "${token}")
         val call = univApi.getUniversities("Bearer ${token}")
 
 
         call.enqueue(object : Callback<UnivResponseDto> {
-            override fun onResponse(call: Call<UnivResponseDto>, response: Response<UnivResponseDto>) {
+            override fun onResponse(
+                call: Call<UnivResponseDto>,
+                response: Response<UnivResponseDto>
+            ) {
                 if (response.isSuccessful) {
                     Log.d("API Request failed", "${response.code()}")
                     val dataResponse = response.body()
                     println(dataResponse)
                     if (dataResponse != null) {
-                        adapter = UnivListAdapter(requireContext(), dataResponse.universitiesPage.contents)
+                        adapter = UnivListAdapter(
+                            requireContext(),
+                            dataResponse.universitiesPage.contents,
+                            this@UnivListPageFragment
+                        )
                     }
                     recyclerView.adapter = adapter
 
@@ -78,6 +91,67 @@ class UnivListPageFragment : Fragment() {
                 println(t)
             }
         })
+    }
+
+    override fun onEditClick(univ: UnivDto) {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.update_univ_dialog, null)
+        val editUnivName = dialogView.findViewById<AppCompatEditText>(R.id.updUnivName)
+        val editUnivCity = dialogView.findViewById<AppCompatEditText>(R.id.updUnivCity)
+        val btnUpdate = dialogView.findViewById<AppCompatButton>(R.id.updateUnivBtn)
+
+        editUnivName?.setText(univ.universityName)
+        editUnivCity?.setText(univ.city)
+
+        val builder = AlertDialog.Builder(context)
+        builder.setView(dialogView)
+        val alertDialog = builder.create()
+        alertDialog.show()
+
+//        btnUpdate.setOnClickListener {
+//            val id = univ.id
+//            val univName = editUnivName?.text.toString()
+//            val city = editUnivCity?.text.toString()
+//            update(UnivDto(id, univName, city))
+//        }
+
+        btnUpdate.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val id = univ.id
+                val univName = editUnivName?.text.toString()
+                val city = editUnivCity?.text.toString()
+                update(UnivDto(id, univName, city))
+            }
+
+        })
+    }
+
+    override fun onDeleteClick(univ: UnivDto) {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun update(updatedUniversity: UnivDto) {
+        val token: String? = SessionManager.getToken(requireContext())
+        Log.d("API Request failed", "${token}")
+        val call = univApi.editUniversity(
+            "Bearer ${token}",
+            updatedUniversity.id.toInt(),
+            updatedUniversity
+        )
+        call.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    Log.d("API Request okay", "${response.code()}")
+                } else {
+                    Log.d("API Request failed", "${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                // Обработка ошибки
+            }
+        })
+
     }
 
 //    private fun getUniversities(recyclerView: RecyclerView, token: String?) {
