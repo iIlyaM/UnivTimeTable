ab5a2b0e097ca2f2611a963b7aab6858f65884af
iIlyaM <imoldavskiy@yandex.ru>
2023-06-02 20:48:30 +0300

    edit ``CreateUserInfoFragment``

--- app/src/main/java/vsu/cs/univtimetable/screens/CreateUserInfoFragment.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/CreateUserInfoFragment.kt
@@ -8,13 +8,20 @@ import android.view.ViewGroup
 import vsu.cs.univtimetable.R
 
 class CreateUserInfoFragment : Fragment() {
+    private val roleType =
+        arrayOf("HEADMAN", "ADMIN", "LECTURER")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_user_info, container, false)
+//        return inflater.inflate(R.layout.fragment_create_user_info, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_user_info, container, false)
+
+
+
+        return view
     }
 
 }
