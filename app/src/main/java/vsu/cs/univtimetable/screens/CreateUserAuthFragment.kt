bba27048e17ede862d616be9fc525922b8d17734
iIlyaM <imoldavskiy@yandex.ru>
2023-06-02 20:48:06 +0300

    edit``CreateUserAuthFragment``: add ``send()``

--- app/src/main/java/vsu/cs/univtimetable/screens/CreateUserAuthFragment.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/CreateUserAuthFragment.kt
@@ -1,10 +1,14 @@
 package vsu.cs.univtimetable.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import vsu.cs.univtimetable.R
 
 class CreateUserAuthFragment : Fragment() {
@@ -14,8 +18,39 @@ class CreateUserAuthFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_user_auth, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_user_auth, container, false)
+        val emailField = view.findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val loginField = view.findViewById<EditText>(R.id.editLoginText)
+        val pwdField = view.findViewById<EditText>(R.id.editTextTextPassword2)
+        val btn = view.findViewById<AppCompatButton>(R.id.confirmBtn)
+
+        btn.setOnClickListener {
+            send(emailField, pwdField, loginField)
+        }
+        return view
+    }
+
+    private fun send(email: EditText, password: EditText, login: EditText) {
+        if (email.text.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email.text.toString())
+                .matches()
+        ) {
+            email.error = "Почта введена некорректно"
+            return
+        }
+        if (password.text.isEmpty()) {
+            password.error = "Вы должны ввести пароль"
+            return
+        }
+        val bundle = Bundle()
+
+        bundle.putString("email", email.text.toString())
+        bundle.putString("password", email.text.toString())
+        bundle.putString("login", email.text.toString())
+
+        findNavController().navigate(
+            R.id.action_createUserAuthFragment_to_createUserInfoFragment,
+            bundle
+        )
     }
 
 
