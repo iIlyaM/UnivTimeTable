77970ea77b50ab0bf5850f7e4aec864d31160b59
iIlyaM <imoldavskiy@yandex.ru>
2023-06-03 00:05:31 +0300

    add ``FacultyApi``: add CRUD methods

---
+++ app/src/main/java/vsu/cs/univtimetable/api/FacultyApi.kt
@@ -0,0 +1,48 @@
+package vsu.cs.univtimetable.api
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+import vsu.cs.univtimetable.dto.FacultyDto
+import vsu.cs.univtimetable.dto.FacultyResponseDto
+import vsu.cs.univtimetable.dto.UnivResponseDto
+
+interface FacultyApi {
+
+    @Headers("Content-Type: application/json")
+    @GET("university/{univId}/faculties")
+    fun getFaculties(
+        @Header("Authorization") basicToken: String,
+        @Path("univId") id: Int,
+        @Query("name") name: String?,
+        @Query("order") order: String?
+    ): Call<FacultyResponseDto>
+
+    @Headers("Content-Type: application/json")
+    @POST("university/{univId}/faculty/create")
+    fun addFaculty(
+        @Header("Authorization") basicToken: String,
+        @Path("univId") id: Int
+    ): Call<Void>
+
+    @Headers("Content-Type: application/json")
+    @PUT("university/faculty/{id}")
+    fun editFaculty(
+        @Header("Authorization") basicToken: String,
+        @Path("id") id: Int,
+        @Body faculty: FacultyDto
+    ): Call<Void>
+
+    @Headers("Content-Type: application/json")
+    @POST("university/faculty/{id}")
+    fun deleteFaculty(
+        @Header("Authorization") basicToken: String,
+        @Path("id") id: Int
+    ): Call<Void>
+}
