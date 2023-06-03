96da7d41e06da992608ed717589740dfe8b64b3b
iIlyaM <imoldavskiy@yandex.ru>
2023-06-03 19:25:56 +0300

    edit dto: ``GroupDto``

--- app/src/main/java/vsu/cs/univtimetable/dto/GroupDto.kt
+++ app/src/main/java/vsu/cs/univtimetable/dto/GroupDto.kt
@@ -1,13 +1,9 @@
 package vsu.cs.univtimetable.dto
-//
-//"id": 0,
-//"groupNumber": 0,
-//"courseNumber": 0,
-//"studentsAmount": 0,
-//"headmanId": 0
+
 data class GroupDto(
-    val id: Int,
+    val id: Int?,
     val groupNumber: Int,
+    val courseNumber:Int,
     val studentsAmount: Int,
-    val headmanId:Int
+    val headman: UserResponse?
 )
