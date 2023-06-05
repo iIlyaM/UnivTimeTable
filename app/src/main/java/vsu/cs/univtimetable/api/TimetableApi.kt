6d736a8487271b29ccdeb785bed90811bd0fc286
iIlyaM <imoldavskiy@yandex.ru>
2023-06-05 15:32:30 +0300

    add ``postSubject()``

--- app/src/main/java/vsu/cs/univtimetable/api/TimetableApi.kt
+++ app/src/main/java/vsu/cs/univtimetable/api/TimetableApi.kt
@@ -1,9 +1,13 @@
 package vsu.cs.univtimetable.api
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import vsu.cs.univtimetable.dto.RequestDataDto
+import vsu.cs.univtimetable.dto.SendRequest
 import vsu.cs.univtimetable.dto.TimetableResponse
 
 interface TimetableApi {
@@ -13,4 +17,17 @@ interface TimetableApi {
     fun getTimetable(
         @Header("Authorization") basicToken: String,
     ): Call<TimetableResponse>
+
+    @Headers("Content-Type: application/json")
+    @GET("request/send")
+    fun getRequestData(
+        @Header("Authorization") basicToken: String,
+    ): Call<RequestDataDto>
+
+    @Headers("Content-Type: application/json")
+    @POST("request/send")
+    fun postSubject(
+        @Header("Authorization") basicToken: String,
+        @Body request: SendRequest
+    ): Call<Void>
 }
