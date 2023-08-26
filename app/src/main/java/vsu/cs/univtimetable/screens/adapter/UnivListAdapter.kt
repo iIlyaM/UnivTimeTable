ba5481b860c116f5f227c985ce2608746824bc85
Ilya <@iIlyaM>
2023-08-26 21:34:48 +0300

    fix ``UnivListAdapter``

--- app/src/main/java/vsu/cs/univtimetable/screens/adapter/UnivListAdapter.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/adapter/UnivListAdapter.kt
@@ -1,85 +1,86 @@
 package vsu.cs.univtimetable.screens.adapter
 
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import vsu.cs.univtimetable.R
-import vsu.cs.univtimetable.dto.UnivDto
+import vsu.cs.univtimetable.databinding.UnivsListItemBinding
+import vsu.cs.univtimetable.dto.univ.UnivDto
 
 
-interface OnUnivItemClickListener {
-    fun onEditClick(univ: UnivDto)
-    fun onDeleteClick(univ: UnivDto)
+interface OnUnivEditInterface {
+    fun onEditClick(id: Int)
+}
 
+interface OnUnivDeleteInterface {
+    fun onDeleteClick(id: Int)
+}
+
+
+interface OnUnivClickInterface {
     fun onItemClick(id: Int)
 }
 
+
 class UnivListAdapter(
-    var context: Context,
-    var univs: List<UnivDto>,
-    val listener: OnUnivItemClickListener
+    private val onEditClick: OnUnivEditInterface,
+    private val onDeleteClick: OnUnivDeleteInterface,
+    private val onItemClick: OnUnivClickInterface,
 ) :
-    RecyclerView.Adapter<UnivListAdapter.UnivViewHolder>() {
-    inner class UnivViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var univNameView: TextView
-        var cityNameView: TextView
-        var settingsBtn: ImageView
-        var deleteBtn: ImageView
+    ListAdapter<UnivDto, UnivListAdapter.ViewHolder>(
+        DiffUtilCallback
+    ) {
 
-        init {
-            itemView.setOnClickListener {
-                listener.onItemClick(univs[adapterPosition].id.toInt())
-            }
-
-            univNameView = itemView.findViewById(R.id.univNameView)
-            cityNameView = itemView.findViewById(R.id.cityNameView)
-            settingsBtn = itemView.findViewById(R.id.icSettingsView)
-            deleteBtn = itemView.findViewById(R.id.icDeleteView)
-
-            settingsBtn.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    listener.onEditClick(univs[position])
-                }
-            }
-
-            deleteBtn.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    listener.onDeleteClick(univs[position])
-                }
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnivViewHolder {
-        return UnivViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.univs_list_item, parent,
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnivListAdapter.ViewHolder {
+        return ViewHolder(
+            UnivsListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
                 false
             )
         )
     }
 
-    override fun onBindViewHolder(holder: UnivViewHolder, position: Int) {
-        val obj = univs[position]
-        holder.univNameView.text = abbreviateUniversityName(obj.universityName).toUpperCase()
-        holder.cityNameView.text = obj.city
-        holder.settingsBtn.setOnClickListener { view ->
-            listener.onEditClick(univs[position])
-        }
-        holder.deleteBtn.setOnClickListener { view ->
-            listener.onDeleteClick(univs[position])
-        }
+    override fun onBindViewHolder(holder: UnivListAdapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    private object DiffUtilCallback : DiffUtil.ItemCallback<UnivDto>() {
+        override fun areItemsTheSame(oldItem: UnivDto, newItem: UnivDto): Boolean =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: UnivDto, newItem: UnivDto): Boolean =
+            oldItem == newItem
     }
 
 
-    override fun getItemCount(): Int {
-        return univs.size
+    inner class ViewHolder(
+        private val binding: UnivsListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(univ: UnivDto) {
+            binding.univNameView.text = abbreviateUniversityName(univ.universityName).toUpperCase()
+            binding.cityNameView.text = univ.city
+
+            binding.icSettingsView.setOnClickListener {
+                val id = getItem(position).id
+                onEditClick.onEditClick(id.toInt())
+            }
+
+            binding.icDeleteView.setOnClickListener {
+                val id = getItem(position).id
+                Log.d("Error", "bind: $id, ${univ.id}")
+                onDeleteClick.onDeleteClick(id.toInt())
+            }
+
+            itemView.setOnClickListener {
+                val id = getItem(position).id
+                onItemClick.onItemClick(id.toInt())
+            }
+        }
+
     }
 
     private fun abbreviateUniversityName(fullName: String): String {
@@ -93,3 +94,80 @@ class UnivListAdapter(
         return abbreviation.toString()
     }
 }
+
+
+//class UnivListAdapter(
+//    var context: Context,
+//    var univs: List<UnivDto>,
+//    val listener: OnUnivItemClickListener
+//) :
+//    RecyclerView.Adapter<UnivListAdapter.UnivViewHolder>() {
+//    inner class UnivViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        var univNameView: TextView
+//        var cityNameView: TextView
+//        var settingsBtn: ImageView
+//        var deleteBtn: ImageView
+//
+//        init {
+//            itemView.setOnClickListener {
+//                listener.onItemClick(univs[adapterPosition].id.toInt())
+//            }
+//
+//            univNameView = itemView.findViewById(R.id.univNameView)
+//            cityNameView = itemView.findViewById(R.id.cityNameView)
+//            settingsBtn = itemView.findViewById(R.id.icSettingsView)
+//            deleteBtn = itemView.findViewById(R.id.icDeleteView)
+//
+//            settingsBtn.setOnClickListener {
+//                val position = adapterPosition
+//                if (position != RecyclerView.NO_POSITION) {
+//                    listener.onEditClick(univs[position])
+//                }
+//            }
+//
+//            deleteBtn.setOnClickListener {
+//                val position = adapterPosition
+//                if (position != RecyclerView.NO_POSITION) {
+//                    listener.onDeleteClick(univs[position])
+//                }
+//            }
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnivViewHolder {
+//        return UnivViewHolder(
+//            LayoutInflater.from(context).inflate(
+//                R.layout.univs_list_item, parent,
+//                false
+//            )
+//        )
+//    }
+//
+//    override fun onBindViewHolder(holder: UnivViewHolder, position: Int) {
+//        val obj = univs[position]
+//        holder.univNameView.text = abbreviateUniversityName(obj.universityName).toUpperCase()
+//        holder.cityNameView.text = obj.city
+//        holder.settingsBtn.setOnClickListener { view ->
+//            listener.onEditClick(univs[position])
+//        }
+//        holder.deleteBtn.setOnClickListener { view ->
+//            listener.onDeleteClick(univs[position])
+//        }
+//    }
+//
+//
+//    override fun getItemCount(): Int {
+//        return univs.size
+//    }
+//
+//    private fun abbreviateUniversityName(fullName: String): String {
+//        val words = fullName.split(" ")
+//        val abbreviation = StringBuilder()
+//        for (word in words) {
+//            if (word.isNotBlank()) {
+//                abbreviation.append(word[0])
+//            }
+//        }
+//        return abbreviation.toString()
+//    }
+//}
