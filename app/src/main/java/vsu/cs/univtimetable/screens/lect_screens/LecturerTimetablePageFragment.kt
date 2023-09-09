6a3147f2ed4f2b1c3041bc9210f4dd50037e01e5
Ilya <@iIlyaM>
2023-09-10 01:32:22 +0300

    fix download timetable button

--- app/src/main/java/vsu/cs/univtimetable/screens/lect_screens/LecturerTimetablePageFragment.kt
+++ app/src/main/java/vsu/cs/univtimetable/screens/lect_screens/LecturerTimetablePageFragment.kt
@@ -1,11 +1,16 @@
 package vsu.cs.univtimetable.screens.lect_screens
 
+import android.Manifest
 import android.app.AlertDialog
 import android.app.ProgressDialog
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -13,7 +18,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
@@ -26,11 +30,11 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import vsu.cs.univtimetable.DateManager.Companion.WEEK_DAYS
-import vsu.cs.univtimetable.DateManager.Companion.checkWeekType
-import vsu.cs.univtimetable.DateManager.Companion.getDayOfWeek
+import vsu.cs.univtimetable.utils.date_utils.DateManager.Companion.WEEK_DAYS
+import vsu.cs.univtimetable.utils.date_utils.DateManager.Companion.checkWeekType
+import vsu.cs.univtimetable.utils.date_utils.DateManager.Companion.getDayOfWeek
 import vsu.cs.univtimetable.R
-import vsu.cs.univtimetable.SessionManager
+import vsu.cs.univtimetable.utils.token_utils.SessionManager
 import vsu.cs.univtimetable.TimetableClient
 import vsu.cs.univtimetable.api.TimetableApi
 import vsu.cs.univtimetable.dto.classes.ClassDto
@@ -40,8 +44,11 @@ import vsu.cs.univtimetable.screens.adapter.DayOfWeekAdapter
 import vsu.cs.univtimetable.screens.adapter.LecturerTimetableAdapter
 import vsu.cs.univtimetable.utils.NotificationManager
 import vsu.cs.univtimetable.utils.NotificationManager.showToastNotification
+import vsu.cs.univtimetable.utils.permissions_utils.PermissionsHandler.checkPermissionsButton
+import vsu.cs.univtimetable.utils.permissions_utils.PermissionsHandler.loadFile
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.time.LocalDate
 import java.time.format.TextStyle
 import java.util.Locale
@@ -102,6 +109,7 @@ class LecturerTimetablePageFragment : Fragment() {
         }
 
         downloadLectTTBtn = view.findViewById(R.id.downloadLectTTBtn)
+        checkPermissionsButton(requireContext(), downloadLectTTBtn)
         downloadLectTTBtn.setOnClickListener {
             downloadLectTTBtn.startAnimation()
             downloadTimetable()
@@ -177,29 +185,54 @@ class LecturerTimetablePageFragment : Fragment() {
         val call = timetableApi.downloadFile("Bearer ${token}")
 
         call.enqueue(object : Callback<ResponseBody> {
-            @RequiresApi(Build.VERSION_CODES.O)
             override fun onResponse(
                 call: Call<ResponseBody>,
                 response: Response<ResponseBody>
             ) {
                 if (response.isSuccessful) {
-                    val file = File(
-                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                        "расписание.xlsx"
-                    )
-                    if (file.exists()) {
-                        file.delete()
-                    }
-                    val inputStream = response.body()?.byteStream()
-                    val outputStream = FileOutputStream(file)
+                    val fileName = "расписание"
+                    val contentResolver = requireContext().contentResolver
 
-                    inputStream?.use { input ->
-                        outputStream.use { output ->
-                            input.copyTo(output)
-                        }
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.Downloads.DISPLAY_NAME, fileName)
+                        put(
+                            MediaStore.Downloads.MIME_TYPE,
+                            "application/vnd.ms-excel"
+                        )
+                        put(MediaStore.Downloads.IS_PENDING, 1)
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOCUMENTS)
                     }
-                    stopAnimation(downloadLectTTBtn)
-                    showToastNotification(requireContext(), "Файл сохранён")
+                    try {
+                        val resolver = requireContext().contentResolver
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            loadFile(contentResolver, resolver, contentValues, response)
+                            stopAnimation(downloadLectTTBtn)
+                            showToastNotification(requireContext(), "Файл сохранен")
+                        } else {
+                            val file = File(
+                                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                                fileName
+                            )
+
+                            val inputStream = response.body()?.byteStream()
+                            val outputStream = FileOutputStream(file)
+
+                            inputStream?.use { input ->
+                                outputStream.use { output ->
+                                    input.copyTo(output)
+                                }
+                            }
+
+                            stopAnimation(downloadLectTTBtn)
+                            showToastNotification(requireContext(), "Файл сохранен")
+                        }
+
+                    } catch (e: IOException) {
+                        stopAnimation(downloadLectTTBtn)
+                        Log.e("Error", "Failed to save file to Downloads directory: ${e.message}")
+                        showToastNotification(requireContext(), "ОШИБКА")
+                    }
                 } else {
                     stopAnimation(downloadLectTTBtn)
                     if (response.code() == 400) {
